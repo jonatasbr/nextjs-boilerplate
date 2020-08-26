ba13409c0c279fc3e92a098fa5fed203d6307a7f
Jonatas Braz <jonatasbraz@gmail.com>
2020-08-26 20:04:05 -0300

    adicionando storybook e criando um storie

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,15 +1,16 @@
 import * as Styles from './styles';
 
-const Main = () => (
+const Main = ({
+  title = 'NextJS Boilerplate',
+  description = 'TypeScript, ReactJS, NextJS e Styled Components',
+}) => (
   <Styles.Wrapper>
     <Styles.Logo
       src="/img/logo.svg"
       alt="Imagem de um átomo e React Avançado escrito ao lado"
     />
-    <Styles.Title>React Avançado</Styles.Title>
-    <Styles.Description>
-      Typescript, ReactJS, NExtJS e Styled Components
-    </Styles.Description>
+    <Styles.Title>{title}</Styles.Title>
+    <Styles.Description>{description}</Styles.Description>
     <Styles.Illustration
       src="/img/hero-illustration.svg"
       alt="Um desenvolvedor de frente para uma tela de código"
