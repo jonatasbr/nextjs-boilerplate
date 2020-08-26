72b181381c1823a7eb151407aa01323e2247aef6
Jonatas Braz <jonatasbraz@gmail.com>
2020-08-26 17:51:55 -0300

    criando primeiro componente e seu estilo

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,20 @@
+import * as Styles from './styles';
+
 const Main = () => (
-  <main>
-    <h1>react avançado</h1>
-  </main>
+  <Styles.Wrapper>
+    <Styles.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado"
+    />
+    <Styles.Title>React Avançado</Styles.Title>
+    <Styles.Description>
+      Typescript, ReactJS, NExtJS e Styled Components
+    </Styles.Description>
+    <Styles.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para uma tela de código"
+    />
+  </Styles.Wrapper>
 );
 
 export default Main;
