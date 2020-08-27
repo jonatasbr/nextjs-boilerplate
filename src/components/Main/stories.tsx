1ec61c43bec852d7d644c61b2f4fe651f8ee61a4
Jonatas Braz <jonatasbraz@gmail.com>
2020-08-26 21:32:20 -0300

    ajustes em storybook

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,3 +1,4 @@
+import { Story, Meta } from '@storybook/react/types-6-0';
 import { withKnobs, text } from '@storybook/addon-knobs';
 import Main from '.';
 
@@ -5,14 +6,21 @@ export default {
   title: 'Main',
   component: Main,
   decorators: [withKnobs],
-};
+} as Meta;
 
-export const Basic = () => (
+export const Basic: Story = () => (
   <Main
     title={text('Title', 'NextJS Boilerplate')}
     description={text(
       'Description',
       'TypeScript, ReactJS, NextJS e Styled Components'
     )}
+  />
+);
+
+export const Secondary: Story = () => (
+  <Main
+    title={text('Title', 'Testing secondary')}
+    description={text('Description', 'Testing test')}
   />
 );
