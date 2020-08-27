4a045f9a638294304e897ac26c679b83f9875769
Jonatas Braz <jonatasbraz@gmail.com>
2020-08-26 22:17:51 -0300

    ajustes para usar apenas essetials do storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,4 @@
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
-  addons: [
-    '@storybook/addon-essentials',
-    '@storybook/addon-links',
-    '@storybook/addon-knobs/register',
-  ],
+  addons: ['@storybook/addon-essentials'],
 };
