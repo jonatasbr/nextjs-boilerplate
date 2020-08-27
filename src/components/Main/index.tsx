48e3bb946e1c34fc53a91cefc299297526e5b4e7
Jonatas Braz <jonatasbraz@gmail.com>
2020-08-27 00:00:04 -0300

    fix em cobertura de testes e build do storybook

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -6,13 +6,13 @@ const Main = ({
 }) => (
   <Styles.Wrapper>
     <Styles.Logo
-      src="/img/logo.svg"
+      src="img/logo.svg"
       alt="Imagem de um átomo e React Avançado escrito ao lado"
     />
     <Styles.Title>{title}</Styles.Title>
     <Styles.Description>{description}</Styles.Description>
     <Styles.Illustration
-      src="/img/hero-illustration.svg"
+      src="img/hero-illustration.svg"
       alt="Um desenvolvedor de frente para uma tela de código"
     />
   </Styles.Wrapper>
